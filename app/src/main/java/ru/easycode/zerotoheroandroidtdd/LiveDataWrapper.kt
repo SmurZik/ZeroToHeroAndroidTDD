88eb78a325e8ad4d5817282de50d54d83095853d
smurz <murca2403@gmail.com>
2024-11-04 14:44:47 +0300

    task17-solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/LiveDataWrapper.kt
@@ -0,0 +1,26 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import androidx.lifecycle.LiveData
+
+interface LiveDataWrapper {
+
+    fun save(bundleWrapper: BundleWrapper.Save)
+
+    fun update(value: UiState)
+
+    fun liveData(): LiveData<UiState>
+
+    class Base(
+        private val liveData: SingleLiveEvent<UiState> = SingleLiveEvent()
+    ) : LiveDataWrapper {
+        override fun save(bundleWrapper: BundleWrapper.Save) {
+            liveData.value?.let { bundleWrapper.save(it) }
+        }
+
+        override fun update(value: UiState) {
+            liveData.value = value
+        }
+
+        override fun liveData(): LiveData<UiState> = liveData
+    }
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,38 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        viewModel = (application as App).viewModel
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) {
+            it.apply(binding.actionButton, binding.progressBar, binding.titleTextView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        viewModel.save(BundleWrapper.Base(outState))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.restore(BundleWrapper.Base(savedInstanceState))
     }
 }

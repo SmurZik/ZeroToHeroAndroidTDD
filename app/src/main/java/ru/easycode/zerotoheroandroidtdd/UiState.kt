18580dee69e95103e24264392c89a66e2da141ad
smurz <murca2403@gmail.com>
2024-11-28 17:08:55 +0300

    task18-solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,29 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import java.io.Serializable
+
+interface UiState : Serializable {
+
+    fun apply(button: Button, textView: TextView, progressBar: ProgressBar)
+
+    object ShowProgress : UiState {
+        override fun apply(button: Button, textView: TextView, progressBar: ProgressBar) {
+            progressBar.visibility = View.VISIBLE
+            textView.visibility = View.GONE
+            button.isEnabled = false
+        }
+    }
+
+    data class ShowData(private val text: String) : UiState {
+        override fun apply(button: Button, textView: TextView, progressBar: ProgressBar) {
+            button.isEnabled = true
+            progressBar.visibility = View.GONE
+            textView.visibility = View.VISIBLE
+            textView.text = text
+        }
+    }
+}

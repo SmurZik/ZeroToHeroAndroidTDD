37f7c02bf5f8e3cff1d73be4f45c2d013bb69246
smurz <murca2403@gmail.com>
2025-02-06 18:40:48 +0300

    task22-solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,39 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var words = arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        words = savedInstanceState?.getStringArrayList("key") ?: arrayListOf()
+        words.forEach {
+            addText(it, binding.contentLayout)
+        }
+
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+            addText(text, binding.contentLayout)
+            words.add(text)
+            binding.inputEditText.text?.clear()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList("key", words)
+    }
+
+    private fun addText(text: String, layout: LinearLayout) {
+        val textView = TextView(this)
+        textView.text = text
+        layout.addView(textView)
     }
 }
